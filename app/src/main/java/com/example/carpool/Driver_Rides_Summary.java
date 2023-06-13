080b0a68b8f34ab81007734b1a758059ad9b843d
ahsansk27 <116421046+ahsansk27@users.noreply.github.com>
2023-06-13 07:19:26 +0500

    Add Carpool Android Code

---
+++ app/src/main/java/com/example/carpool/Driver_Rides_Summary.java
@@ -0,0 +1,55 @@
+package com.example.carpool;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Html;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Driver_Rides_Summary extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver__rides__summary);
+
+    }
+        @Override
+        public void onBackPressed() {
+            // super.onBackPressed();
+
+
+            //    super.onBackPressed();
+            AlertDialog.Builder builder = new AlertDialog.Builder(Driver_Rides_Summary.this);
+            builder.setCancelable(false);
+            builder.setMessage("Do you want to Exit?");
+            builder.setPositiveButton(Html.fromHtml("<font color='#000000'>Yes</font>"), new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    //if user pressed "yes", then he is allowed to exit from application
+//                finish();
+//                android.os.Process.killProcess(android.os.Process.myPid());
+//                System.exit(1);
+                    Intent i  = new Intent (Driver_Rides_Summary.this,MainActivity.class);
+                                startActivity(i);
+                                finish();
+
+                }
+            });
+            builder.setNegativeButton(Html.fromHtml("<font color='#000000'>No</font>"), new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    //if user select "No", just cancel this dialog and continue with app
+                    dialog.cancel();
+                }
+            });
+            AlertDialog alert = builder.create();
+            alert.show();
+
+        }
+    }
+
---
+++ app/src/main/java/com/example/carpool/LogIn.java
@@ -0,0 +1,174 @@
+package com.example.carpool;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.dx.dxloadingbutton.lib.LoadingButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class LogIn extends AppCompatActivity {
+
+
+    private FirebaseDatabase database;
+    private DatabaseReference ref;
+    private FirebaseAuth mAuth;
+
+
+
+    private FirebaseAuth.AuthStateListener mAutL = new FirebaseAuth.AuthStateListener() {
+        @Override
+        public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+
+        }
+
+
+    };
+    private static final String TAG = "LogIn";
+
+    private EditText email;
+    private EditText pass;
+    private Button login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);
+
+        email = (EditText) findViewById(R.id.EtEmail);
+        pass = (EditText) findViewById(R.id.EtPass);
+        login = (Button) findViewById(R.id.Login);
+
+
+        // Forgot Password
+        TextView textViewF = (TextView) findViewById(R.id.ForgotPasswrod);
+
+
+
+        textViewF.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent intentF = new Intent(LogIn.this, ForgotPassword.class);
+                startActivity(intentF);
+                Toast.makeText(LogIn.this,"Forgot Password", Toast.LENGTH_LONG).show();
+            }
+
+        });
+
+
+        mAuth = FirebaseAuth.getInstance();
+
+        // create Account Intent
+        TextView signup = (TextView) findViewById(R.id.CreateAccount);
+        signup.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intentS = new Intent(LogIn.this, SignUp.class);
+                startActivity(intentS);
+            }
+        });
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               final String emailt = email.getText().toString();
+                String passt = pass.getText().toString();
+                // login.startLoading();
+
+                if (!emailt.equals("") && !pass.equals("")) {
+                    mAuth.signInWithEmailAndPassword(emailt, passt)
+                            .addOnCompleteListener(LogIn.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+
+
+                                   // String emailPattern = "[0-9]+@[s]+[t]+[u]+[d]+[e]+[n]+[t][s]+\\.+[a]+[u]+\\.+[e]+[d]+[u]+\\.+[p]+[k]+";
+                                    if (!task.isSuccessful()) {
+                                        Toast.makeText(LogIn.this, "Wrong Email or Password", Toast.LENGTH_LONG).show();
+                                        //ref.setValue("NOOOOOOO");
+                                    } else {
+                                        // Toast.makeText(LogIn.this, "Welcome", Toast.LENGTH_LONG).show();
+                                        //ref.setValue("Hello");
+                                        checkIfEmailVerified();
+                                    }
+                                }
+                            });
+
+                }
+            }
+        });
+
+
+    }
+
+
+
+
+    private void checkIfEmailVerified() {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        if (user.isEmailVerified())
+        {
+            // user is verified, so you can finish this activity or send user to activity which you want.
+            finish();
+            Toast.makeText(LogIn.this, "Successfully logged in", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(LogIn.this,SignIn.class);
+           // login.loadingSuccessful();
+
+                    startActivity(intent);
+
+        }
+        else
+        {
+            // email is not verified, so just prompt the message to the user and restart this activity.
+            // NOTE: don't forget to log out the user.
+            Toast.makeText(LogIn.this, "Verify your Email First", Toast.LENGTH_SHORT).show();
+            FirebaseAuth.getInstance().signOut();
+
+            //restart this activity
+
+        }
+
+
+
+    }
+
+}
+
+
+
+    //Forgot Password
+
+
+
+      /*  // Log In Button
+        Button buttonL = (Button) findViewById(R.id.LogIn);
+
+        buttonL.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent intentL = new Intent(LogIn.this, SignIn.class);
+                startActivity(intentL);
+            }
+
+        });
+
+
+
+    }
+}
+*/
